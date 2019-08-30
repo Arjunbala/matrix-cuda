98d4f3a891c242a97dd7f35fedbbe60a62eaa556
Arjun Balasubramanian <yuhanl@c240g5-110219.wisc.cloudlab.us>
2019-08-30 11:58:07 -0500

    DR model simulation

---
+++ dr_model_poc.cu
@@ -0,0 +1,221 @@
+/*
+ *  file name: matrix.cu
+ *
+ *  matrix.cu contains the code that realize some common used matrix operations in CUDA
+ *  
+ *  this is a toy program for learning CUDA, some functions are reusable in other project
+ *  
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#ifndef BLOCK_SIZE
+    #define BLOCK_SIZE 16
+#endif
+
+#ifndef BATCH_SIZE
+    #define BATCH_SIZE 1
+#endif
+
+#ifndef NUM_ITERATIONS
+    #define NUM_ITERATIONS 1024
+#endif
+
+/*
+*********************************************************************
+function name: gpu_matrix_mult
+
+description: dot product of two matrix (not only square)
+
+parameters: 
+            &a GPU device pointer to a m X n matrix (A)
+            &b GPU device pointer to a n X k matrix (B)
+            &c GPU device output purpose pointer to a m X k matrix (C) 
+            to store the result
+
+Note:
+    grid and block should be configured as:
+        dim3 dimGrid((k + BLOCK_SIZE - 1) / BLOCK_SIZE, (m + BLOCK_SIZE - 1) / BLOCK_SIZE);
+        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+
+    further sppedup can be obtained by using shared memory to decrease global memory access times
+return: none
+*********************************************************************
+*/
+__global__ void gpu_matrix_mult(int *a,int *b, int *c, int m, int n, int k)
+{
+    int row = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int sum = 0;
+    if( col < k && row < m) 
+    {
+        for(int i = 0; i < n; i++) 
+        {
+            sum += a[row * n + i] * b[i * k + col];
+        }
+        c[row * k + col] = sum;
+    }
+} 
+
+/*
+*********************************************************************
+function name: main
+
+description: test and compare
+
+parameters: 
+            none
+
+return: none
+*********************************************************************
+*/
+int main(int argc, char const *argv[])
+{
+    int m1, n1, k1, m2, n2, k2, m3, n3, k3;
+
+    /* Fixed seed for illustration */
+    srand(3333);
+    
+    m1=BATCH_SIZE;
+    n1=65536;
+    k1=4096;
+
+    m2=BATCH_SIZE;
+    n2=4096;
+    k2=1024;
+
+    m3=BATCH_SIZE;
+    n3=4096;
+    k3=10; 
+
+    // allocate memory in host RAM
+    int *h_a, *h_b, *h_c, *h_d, *h_e, *h_f, *h_g;
+    cudaMallocHost((void **) &h_a, sizeof(int)*m1*n1);
+    cudaMallocHost((void **) &h_b, sizeof(int)*n1*k1);
+    cudaMallocHost((void **) &h_c, sizeof(int)*m1*k1);
+    cudaMallocHost((void **) &h_d, sizeof(int)*n2*k2);
+    cudaMallocHost((void **) &h_e, sizeof(int)*m2*k2);
+    cudaMallocHost((void **) &h_f, sizeof(int)*n3*k3);
+    cudaMallocHost((void **) &h_g, sizeof(int)*m3*k3);
+
+    // random initialize matrix B
+    for (int i = 0; i < n1; ++i) {
+        for (int j = 0; j < k1; ++j) {
+            h_b[i * k1 + j] = rand() % 1024;
+        }
+    }
+
+    // random initialize matrix D
+    for (int i = 0; i < n2; ++i) {
+        for (int j = 0; j < k2; ++j) {
+            h_d[i * k2 + j] = rand() % 1024;
+        }
+    }
+
+    // random initialize matrix F
+    for (int i = 0; i < n3; ++i) {
+        for (int j = 0; j < k3; ++j) {
+            h_f[i * k3 + j] = rand() % 1024;
+        }
+    }
+
+    float gpu_elapsed_time_ms;
+
+    // some events to count the execution time
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // Allocate memory space on the device 
+    int *d_a, *d_b, *d_c, *d_d, *d_e, *d_f, *d_g;
+    cudaMalloc((void **) &d_a, sizeof(int)*m1*n1);
+    cudaMalloc((void **) &d_b, sizeof(int)*n1*k1);
+    cudaMalloc((void **) &d_c, sizeof(int)*m1*k1);
+    cudaMalloc((void **) &d_d, sizeof(int)*n2*k2);
+    cudaMalloc((void **) &d_e, sizeof(int)*m2*k2);
+    cudaMalloc((void **) &d_f, sizeof(int)*n3*k3);
+    cudaMalloc((void **) &d_g, sizeof(int)*m3*k3);
+
+    // copy matrix B,D,F from host to device memory - these are weight matrices
+    cudaMemcpy(d_b, h_b, sizeof(int)*n1*k1, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_d, h_d, sizeof(int)*n2*k2, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_f, h_f, sizeof(int)*n3*k3, cudaMemcpyHostToDevice);
+
+    int numExamples = 0;
+    double total_time_ms = 0.0;
+    for(int i=0;i<NUM_ITERATIONS;i++) {
+        // random initialize matrix A - this is the input matrix
+        for (int i = 0; i < m1; ++i) {
+            for (int j = 0; j < n1; ++j) {
+                h_a[i * n1 + j] = rand() % 1024;
+            }
+        }
+	cudaEventRecord(start, 0);
+	// copy from host to device
+        cudaMemcpy(d_a, h_a, sizeof(int)*m1*n1, cudaMemcpyHostToDevice);
+	
+	unsigned int grid_rows = (m1 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        unsigned int grid_cols = (k1 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        dim3 dimGrid(grid_cols, grid_rows);
+        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+
+        // Launch kernel for multiplication 1
+#ifdef USE_CUDA_STREAMS
+	gpu_matrix_mult<<<dimGrid, dimBlock, 0, 0>>>(d_a, d_b, d_c, m1, n1, k1); // execute on default stream
+#else
+        gpu_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, m1, n1, k1);
+#endif
+        cudaDeviceSynchronize();
+
+        // Launch kernel for multiplication 2
+        grid_rows = (m2 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        grid_cols = (k2 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        dim3 dimGrid2(grid_cols, grid_rows);
+        dim3 dimBlock2(BLOCK_SIZE, BLOCK_SIZE);
+#ifdef USE_CUDA_STREAMS
+	gpu_matrix_mult<<<dimGrid2, dimBlock2, 0, 0>>>(d_c, d_d, d_e, m2, n2, k2); // execute on default stream
+#else
+        gpu_matrix_mult<<<dimGrid2, dimBlock2>>>(d_c, d_d, d_e, m2, n2, k2);
+#endif
+
+	// Launch kernel for multiplication 3 - DR model
+        grid_rows = (m3 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        grid_cols = (k3 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        dim3 dimGrid3(grid_cols, grid_rows);
+        dim3 dimBlock3(BLOCK_SIZE, BLOCK_SIZE);
+#ifdef USE_CUDA_STREAMS
+	cudaStream_t streams[1];
+	cudaStreamCreate(&streams[0]);
+	gpu_matrix_mult<<<dimGrid3, dimBlock3, 0, streams[0]>>>(d_c, d_f, d_g, m3, n3, k3); // execute on non-default stream
+#else
+        gpu_matrix_mult<<<dimGrid3, dimBlock3>>>(d_c, d_f, d_g, m3, n3, k3);
+#endif
+
+        // Transfer results from device to host - only DR model result
+        cudaMemcpy(h_g, d_g, sizeof(int)*m2*k2, cudaMemcpyDeviceToHost);
+        cudaThreadSynchronize();
+        // time counting terminate
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+
+        // compute time elapse on GPU computing
+        cudaEventElapsedTime(&gpu_elapsed_time_ms, start, stop);
+	numExamples += BATCH_SIZE;
+	total_time_ms += gpu_elapsed_time_ms;
+    }
+    printf("Avg. Latency: %g ms :: Avg. Throughput: %g examples/sec\n", total_time_ms/NUM_ITERATIONS, numExamples*1000.0/total_time_ms);
+
+    // free memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    cudaFree(d_d);
+    cudaFree(d_e);
+    cudaFreeHost(h_a);
+    cudaFreeHost(h_b);
+    cudaFreeHost(h_c);
+    cudaFreeHost(h_d);
+    cudaFreeHost(h_e);
+    return 0;
+}
